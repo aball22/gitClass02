5e1ac09ca903e90f813d699c6af864cb3f7c2077
aball22 <ballaj1221@gmail.com>
2024-11-07 15:05:43 -0700

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,11 +2,8 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("This is the project version 2");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
-        System.out.println("Hello world");
+        System.out.println("This is the project version 3");
+
     }
 }
-// done with version two
+// done with version three
